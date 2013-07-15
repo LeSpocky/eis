fa5e2cec7c7fdd355a9c19ff4be54fb1efd0a30d
Yves Schumann <yves@eisfair.org>
2013-07-15 19:49:39 +0200

    AL-19 - Typos fixen

--- cui-php-apache2/cui-php-apache2-update.sh
+++ cui-php-apache2/cui-php-apache2-update.sh
@@ -1,34 +1,27 @@
 #!/bin/sh
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # eisfair-ng configuration parameter update script
 # Copyright (c) 2007 - 2013 the eisfair team, team(at)eisfair(dot)org
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
-# name of the current package
-# ---------------------------
-packages_name=php-apache2
+package_name=php-apache2
 
-# include libs for using
-# ----------------------
-. /var/install/include/configlib     
+# Include required libs
+. /var/install/include/configlib
 
-### -------------------------------------------------------------------------
-### read old configuration and rename old variables
-### -------------------------------------------------------------------------
-# set the defaults from default.d file
-. /etc/default.d/${packages_name}
-# read old values if exists
-[ -f /etc/config.d/${packages_name} ] && . /etc/config.d/${packages_name}
+# Set defaults from default.d file
+. /etc/default.d/${package_name}
+# Read old values if exists
+[ -f /etc/config.d/${package_name} ] && . /etc/config.d/${package_name}
 
-### -------------------------------------------------------------------------
+### --------------------------------------------------------------------------
 ### Write the new config
-### -------------------------------------------------------------------------
 (
-    printgpl "$packages_name" "2006-07-31" "schlotze" "2008-2013 Sebastian Scholze <sebastian@eisfair.org>"
+    printgpl "$package_name" "2006-07-31" "schlotze" "2008-2013 Sebastian Scholze <sebastian@eisfair.org>"
 
-    #------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
     printgroup  "General settings"
-    #------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
 
     printvar "PHP_MAX_EXECUTION_TIME" "Time in sec. until the script will be"
     printvar "" "terminated."
@@ -38,7 +31,7 @@ packages_name=php-apache2
     printvar "" "in your Browser."
     printvar "" "yes=ON (default) / no=OFF"
 
-    printvar "PHP_LOG_ERROR" "Write Error to an logfile"
+    printvar "PHP_LOG_ERROR" "Write Error to a logfile"
     printvar "" "yes=ON / no=OFF (default)"
 
     printvar "PHP_INCLUDE_PATH" "Path were include files are located"
@@ -62,17 +55,17 @@ packages_name=php-apache2
     
     printvar "PHP_DATE_TIMEZONE" "Enter your timezone here"
 
-    #------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
     printgroup "Info Settings"
-    #------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
 
     printvar "PHP_INFO" "Puts some PHP-Scripts in your htdoc-dir"
     printvar "" "for testing functionallity for"
     printvar "" "php, gd, pdf."
 
-    #------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
     printgroup "Memory Settings"
-    #------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
 
     printvar "PHP_MAX_POST_SIZE" "Maximal POST size"
     printvar "" "If you use the POST-Method for uploads"
@@ -89,40 +82,38 @@ packages_name=php-apache2
     printvar "PHP_UPLOAD_DIR" "Where to temporary save uploaded file"
     printvar "" "default: /tmp"
 
-    #------------------------------------------------------------------------------
-    printgroup "EXTENSION CONFIGURATION - CACHE/OTHER"
-    #------------------------------------------------------------------------------
-    printvar "PHP_EXT_CACHE" "Activate chaching module in PHP."
+    # ------------------------------------------------------------------------
+    printgroup "Extension Configuration - Cache/other"
+    # ------------------------------------------------------------------------
+    printvar "PHP_EXT_CACHE" "Activate caching module in PHP."
     printvar "" "apc: for APC"
     printvar "" "xcache: for XCache"
-    printvar "" "default: no -> switch chaching module off"
+    printvar "" "default: no -> switch caching module off"
     printvar "PHP_EXT_SOAP" "Activate SOAP module in PHP."
 
-    #------------------------------------------------------------------------------
-    printgroup "EXTENSION CONFIGURATION - DATABASE"
-    #------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
+    printgroup "Extension Configuration - Database"
+    # ------------------------------------------------------------------------
 
-    printvar "PHP_EXT_MYSQL" "include MySQL extension in PHP"
-    printvar "PHP_EXT_MYSQL_SOCKET" "default socket to connect the"
-    printvar "" "MySQL Database."
-    printvar "" "default  /run/mysqld/mysqld.sock"
+    printvar "PHP_EXT_MYSQL" "Include MySQL extension in PHP"
+    printvar "PHP_EXT_MYSQL_SOCKET" "Default socket to connect the MySQL Database. Default  /run/mysqld/mysqld.sock"
     printvar "PHP_EXT_MYSQL_HOST" "Hostname or IP address if use port connect"
     printvar "PHP_EXT_MYSQL_PORT" "MySQL connect port (3306)"
-    printvar "PHP_EXT_MSSQL" "include msSQL extension in PHP"
-    printvar "PHP_EXT_PGSQL" "include PostgreSQL extension in PHP"
-    printvar "PHP_EXT_INTER" "include INTERBASE extension in PHP"
-    printvar "PHP_EXT_SQLITE3" "include SQLite3 extension in PHP"
+    printvar "PHP_EXT_MSSQL" "Include msSQL extension in PHP"
+    printvar "PHP_EXT_PGSQL" "Include PostgreSQL extension in PHP"
+    printvar "PHP_EXT_INTER" "Include INTERBASE extension in PHP"
+    printvar "PHP_EXT_SQLITE3" "Include SQLite3 extension in PHP"
     printvar "PHP_EXT_LDAP" "Activate LDAP module in PHP."
-    #------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
     printend
-    #------------------------------------------------------------------------------
-) > /etc/config.d/${packages_name}
+    # ------------------------------------------------------------------------
+) > /etc/config.d/${package_name}
 # Set rights
-chmod 0644  /etc/config.d/${packages_name}
-chown root  /etc/config.d/${packages_name}
+chmod 0644  /etc/config.d/${package_name}
+chown root  /etc/config.d/${package_name}
 
 
-### ---------------------------------------------------------------------------
+### --------------------------------------------------------------------------
 ### end
-### ---------------------------------------------------------------------------
+### --------------------------------------------------------------------------
 
