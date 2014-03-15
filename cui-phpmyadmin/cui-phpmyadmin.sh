bb9ebbdeae651ff4688c14e112f2183d1ae7cf08
Yves Schumann <yves@eisfair.org>
2014-03-15 13:18:39 +0100

    AL-43: First step towards fix of generated configuration file

--- cui-phpmyadmin/cui-phpmyadmin.sh
+++ cui-phpmyadmin/cui-phpmyadmin.sh
@@ -428,16 +428,6 @@ EOF
             cat >> ${configPhp} <<EOF
 /* Server ${host} (${fileconf}) [${idx1}] */
 \$i++;
-\$cfg['Servers'][\$i]['bookmarktable']      = "pma_bookmark";
-\$cfg['Servers'][\$i]['relation']           = "pma_relation";
-\$cfg['Servers'][\$i]['table_info']         = "pma_table_info";
-\$cfg['Servers'][\$i]['table_coords']       = "pma_table_coords";
-\$cfg['Servers'][\$i]['pdf_pages']          = "pma_pdf_pages";
-\$cfg['Servers'][\$i]['column_info']        = "pma_column_info";
-\$cfg['Servers'][\$i]['history']            = "pma_history";
-\$cfg['Servers'][\$i]['designer_coords']    = "pma_designer_coords";
-
-
 \$cfg['Servers'][\$i]['host']               = "${host}";
 \$cfg['Servers'][\$i]['port']               = "${port}";
 \$cfg['Servers'][\$i]['user']               = "${user}";
@@ -490,6 +480,16 @@ EOF
 \$cfg['Servers'][\$i]['pmadb']              = "${pmadb}";
 \$cfg['Servers'][\$i]['controluser']        = "${controluser}";
 \$cfg['Servers'][\$i]['controlpass']        = "${controlpass}";
+
+\$cfg['Servers'][\$i]['bookmarktable']      = "pma_bookmark";
+\$cfg['Servers'][\$i]['relation']           = "pma_relation";
+\$cfg['Servers'][\$i]['table_info']         = "pma_table_info";
+\$cfg['Servers'][\$i]['table_coords']       = "pma_table_coords";
+\$cfg['Servers'][\$i]['pdf_pages']          = "pma_pdf_pages";
+\$cfg['Servers'][\$i]['column_info']        = "pma_column_info";
+\$cfg['Servers'][\$i]['history']            = "pma_history";
+\$cfg['Servers'][\$i]['designer_coords']    = "pma_designer_coords";
+
 \$cfg['Servers'][\$i]['ssl']                = ${useSSL};
 \$cfg['Servers'][\$i]['nopassword']         = ${noPass};
 
