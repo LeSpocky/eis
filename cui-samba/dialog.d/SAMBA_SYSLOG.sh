ee58e89cf9bed4ca2070cd565fb70231e20fd1e3
Yves Schumann <yves@eisfair.org>
2014-09-19 23:08:28 +0200

    AL-35: Added initial content to check file; Added dialog scripts

---
+++ cui-samba/dialog.d/SAMBA_SYSLOG.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+#-----------------------------------------------------------------------------
+# /var/install/dialog.d/SAMBA_SYSLOG.sh - script dialog for ece
+#
+# Creation:     2014-09-19 starwarsfan
+#
+# Copyright (c) 2014 the eisfair team <team@eisfair.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#-----------------------------------------------------------------------------
+. /var/install/include/cuilib
+. /var/install/include/ecelib
+
+#-----------------------------------------------------------------------------
+# exec_dialog
+# ece --> request to create and execute dialog
+#         $p2 --> main window handle
+#         $p3 --> name of config variable
+#-----------------------------------------------------------------------------
+exec_dialog()
+{
+    win="${p2}"
+    sellist="1,2,3,4,5"
+    ece_select_list_dlg "${win}" "${p3}" "${sellist}"
+}
+
+#-----------------------------------------------------------------------------
+# main routine
+#-----------------------------------------------------------------------------
+cui_init
+cui_run
+
+#-----------------------------------------------------------------------------
+# end
+#-----------------------------------------------------------------------------
+exit 0
