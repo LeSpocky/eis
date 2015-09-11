39e889260932b140a5f062da9ae1bf5500e309a8
Yves Schumann <yves.schumann@ti8m.ch>
2015-09-11 15:23:39 +0200

    AL-73: Updated configuration

--- cui-inadyn/dialog.d/INADYN_SYSTEM_CUI.sh
+++ cui-inadyn/dialog.d/INADYN_SYSTEM_CUI.sh
@@ -14,7 +14,7 @@
 exec_dialog()
 {
     win="$p2"
-    sellist="dynamic,static,custom,zoneedit,no-ip,changeip"
+    sellist="dynamic,static,custom,dyndns.org,freedns.afraid.org,zoneedit.com,no-ip.com,easydns.com,tzo.com,3322.org,dnsomatic.com,tunnelbroker.net,dns.he.net/,dynsip.org,sitelutions.com,dnsexit.com,changeip.com,zerigo.com,dhis.org,nsupdate.info,duckdns.org,loopia.com,namecheap.com,domains.google.com,ovh.com,dtdns.com,giradns.com,duiadns.net,"
     ece_select_list_dlg "$win" "Select used dynamic DNS system" "$sellist"
 }
 
