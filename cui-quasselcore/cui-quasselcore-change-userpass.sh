71f4fa32e62ccdffe6d3bb190865ee8ea4b69d4f
Jens Vehlhaber <jens@eisfair.org>
2016-02-07 18:41:25 +0100

    fix config file name

--- cui-quasselcore/cui-quasselcore-change-userpass.sh
+++ cui-quasselcore/cui-quasselcore-change-userpass.sh
@@ -2,12 +2,12 @@
 # ---------------------------------------------------------------------------
 # /var/install/bin/cui-quassel-core-change-userpass.sh - change password of a
 # quasselcore user
-# Copyright (c) 2007 - 2014 the eisfair team, team(at)eisfair(dot)org
+# Copyright (c) 2007 - 2016 the eisfair team, team(at)eisfair(dot)org
 #----------------------------------------------------------------------------
 
 # Load eislib and quasselcore configuration
 . /var/install/include/eislib
-. /etc/config.d/cui-quassel-core
+. /etc/config.d/quasselcore
 
 # ask for username and call change-pass function with it
 USER_INPUT=`/var/install/bin/ask "Name of User to change Password" "" "+"`
