65ac20b5e8c93542f73979dcb9a14091661558e6
Yves Schumann <yves@eisfair.org>
2016-10-15 23:09:00 +0200

    Fixed command setup

--- cui-acme/cui-acme.sh
+++ cui-acme/cui-acme.sh
@@ -46,14 +46,14 @@ generateNewCert()
                             fi
                             idx3=$((idx3+1))
                         done
-                        command="sh /usr/bin/acme.sh --issue ${domainsToGetCertFor} -w ${currentWebroot} --home /etc/ssl/acme 2>&1"
+                        command="sh /usr/bin/acme.sh --issue ${domainsToGetCertFor} -w ${currentWebroot} --home /etc/ssl/acme"
                         echo "$(date "+%Y-%m-%d %H:%M:%S") ${command}"
-                        ${command}
+                        ${command} 2>&1
                         rtc=$?
                         if [ ${rtc} -eq 0 ] ; then
-                            command="sh /usr/bin/acme.sh --installcert -d ${currentDomain} --home /etc/ssl/acme --certpath /etc/ssl/certs/${currentDomain}.pem --keypath /etc/ssl/private/${currentDomain}.key --capath /etc/ssl/certs/${currentDomain}.ca.pem --reloadcmd 'rc-service apache2 restart' 2>&1"
+                            command="sh /usr/bin/acme.sh --installcert -d ${currentDomain} --home /etc/ssl/acme --certpath /etc/ssl/certs/${currentDomain}.pem --keypath /etc/ssl/private/${currentDomain}.key --capath /etc/ssl/certs/${currentDomain}.ca.pem --reloadcmd 'rc-service apache2 restart'"
                             echo "$(date "+%Y-%m-%d %H:%M:%S") ${command}"
-                            ${command}
+                            ${command} 2>&1
                             if [ ${rtc} -ne 0 ] ; then
                                 echo "$(date "+%Y-%m-%d %H:%M:%S") WARN: Installing certs returned with exit code $rtc)!"
                             fi
