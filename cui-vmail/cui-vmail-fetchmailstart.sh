1e8a256f4c0b1fe6c2420aaca228165f45c9b20b
Jens Vehlhaber <jens@eisfair.org>
2017-01-31 09:58:33 +0100

    Yves: Added parameters to fetchmail call to fix interaction with milter if there are more than one message

--- cui-vmail/cui-vmail-fetchmailstart.sh
+++ cui-vmail/cui-vmail-fetchmailstart.sh
@@ -14,7 +14,7 @@ fi
 fetchfile=".fetchmailrc.$$"
 
 /usr/bin/mysql2fetchmail -t /var/spool/postfix/virtual/${fetchfile} -a 0 -u $VMAIL_SQL_USER -s $VMAIL_SQL_HOST -d $VMAIL_SQL_DATABASE -p $VMAIL_SQL_PASS -e $VMAIL_SQL_ENCRYPT_KEY
-/usr/bin/fetchmail -t ${FETCHMAIL_TIMEOUT} -f /var/spool/postfix/virtual/$fetchfile $logging --nobounce --sslcertpath /etc/ssl/certs 2>/dev/null
+/usr/bin/fetchmail -t ${FETCHMAIL_TIMEOUT} -f /var/spool/postfix/virtual/$fetchfile $logging -B 300 -b 1 --nobounce --sslcertpath /etc/ssl/certs 2>/dev/null
 
 rm -f /var/spool/postfix/virtual/$fetchfile
 
